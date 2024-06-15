fa1c63f17f6f4fdd6d7a3c36227e7a2a2de72336
Mon | Cumulo <moncho.santosdios@gmail.com>
2024-06-15 21:26:06 +0200

    Create fuelmetrics.sh

---
+++ fuel_metrics/fuelmetrics.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# node_exporter metrics file path
+metrics_file="/usr/local/metrics/node_exporter_metrics.prom"
+
+# Obtain fuel height
+height=$(curl -X POST http://0.0.0.0:5000/v1/graphql \
+  -H "Content-Type: application/json" \
+  -d '{"query": "{ chain { latestBlock { id height } } }"}' | jq -r '.data.chain.latestBlock.height')
+
+# Define HELP and TYPE
+help_comment="# HELP fuel_height Fuel node block height"
+type_comment="# TYPE fuel_height gauge"
+
+# save
+{
+    echo "$help_comment"
+    echo "$type_comment"
+    echo "fuel_height $height"
+} >> "$metrics_file"
