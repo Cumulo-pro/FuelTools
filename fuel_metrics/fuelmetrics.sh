60cf0e90873ef161e9a1bf489fddaf5ca4e92608
Mon | Cumulo <moncho.santosdios@gmail.com>
2024-06-16 12:20:47 +0200

    Update fuelmetrics.sh

--- fuel_metrics/fuelmetrics.sh
+++ fuel_metrics/fuelmetrics.sh
@@ -12,9 +12,16 @@ height=$(curl -X POST http://0.0.0.0:5000/v1/graphql \
 help_comment="# HELP fuel_height Fuel node block height"
 type_comment="# TYPE fuel_height gauge"
 
+# Define HELP and TYPE for block id
+help_comment_id="# HELP fuel_block_id Fuel node block id"
+type_comment_id="# TYPE fuel_block_id gauge"
+
 # Overwrite the metrics file with the new data
 {
-    echo "$help_comment"
-    echo "$type_comment"
+    echo "$help_comment_height"
+    echo "$type_comment_height"
     echo "fuel_height $height"
+    echo "$help_comment_id"
+    echo "$type_comment_id"
+    echo "fuel_block_id $block_id"
 } > "$metrics_file"
