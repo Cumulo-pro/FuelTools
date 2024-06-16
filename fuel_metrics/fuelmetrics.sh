91a64629efbcf63fe4cc05c2486c9444e097c30f
Mon | Cumulo <moncho.santosdios@gmail.com>
2024-06-16 13:33:15 +0200

    Update fuelmetrics.sh

--- fuel_metrics/fuelmetrics.sh
+++ fuel_metrics/fuelmetrics.sh
@@ -11,7 +11,6 @@ response=$(curl -X POST http://0.0.0.0:5000/v1/graphql \
 height=$(echo "$response" | jq -r '.data.chain.latestBlock.height')
 block_id=$(echo "$response" | jq -r '.data.chain.latestBlock.id')
 
-
 # Define HELP and TYPE for height
 help_comment_height="# HELP fuel_height Fuel node block height"
 type_comment_height="# TYPE fuel_height gauge"
@@ -25,5 +24,5 @@ help_comment_id="# HELP fuel_block_id Fuel node block id"
     echo "$type_comment_height"
     echo "fuel_height $height"
     echo "$help_comment_id"
-    echo "id{block_id=\"$block_id\"} $height"
+    echo "fuel_block_info{block_id=\"$block_id\"} $height"
 } > "$metrics_file"
